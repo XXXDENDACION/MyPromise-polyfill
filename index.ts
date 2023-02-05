6a3f18481533226bbb46a318ab0a97c18050baec
Denis <icesfrost@gmail.com>
2023-02-06 00:28:19 +0300

    Add all, allSettled, race, resolve, reject static methods

--- index.ts
+++ index.ts
@@ -1,17 +1,28 @@
 import {asap, isPromiseLike} from "./utils";
 
 type Executor<T> = (
-  resolve: Resolve<T>,
+  resolve: Resolve,
   reject: Reject
 ) => void;
 
-type Callback<T> = [AnyFunction | undefined, AnyFunction | undefined, Resolve<T>, Reject];
-type AnyFunction = (...args: any[]) => any;
-type Resolve<T> = (value: T) => void;
+type Resolve = (value: any) => void;
 type Reject = (reason?: any) => void;
+type ThenCb<T> = (value: T) => any;
+type CatchCb = (reason?: any) => any;
+
+type AllSettledResult<T> =
+    | {
+    status: "fulfilled";
+    value: T;
+}
+    | {
+    status: "rejected";
+    reason: any;
+};
+
 type Status = 'fulfilled' | 'rejected' | 'pending';
 class MyPromise<T> {
-  thenCbs: Callback<T>[] = [];
+  thenCbs: [ThenCb<T> | undefined, CatchCb | undefined, Resolve, Reject][] = [];
   status: Status = 'pending';
   value: T | null = null;
   error?: any;
@@ -24,16 +35,83 @@ class MyPromise<T> {
       }
   }
 
-  public then = (thenCallback: (value: T) => void, catchCallback?: (reason?: any) => void) => {
+  static all<U>(arrayPromises: (U | MyPromise<U>)[]) {
+      const result: U[] = Array(arrayPromises.length);
+      let count = 0;
+
+      return new MyPromise<U[]>((resolve, reject) => {
+          arrayPromises.forEach((promise, index) => {
+              MyPromise.resolve(promise)
+                  .then(value => {
+                      result[index] = value;
+                      count++;
+
+                      if (count === arrayPromises.length) {
+                        resolve(result);
+                      }
+                  })
+                  .catch((error) => {
+                      reject(error);
+                  })
+          })
+      });
+  }
+
+  static allSettled<U>(arrayPromises: MyPromise<U>[]) {
+      return MyPromise.all<AllSettledResult<U>>(
+          arrayPromises.map((promise) =>
+              promise
+                  .then((value) => ({ status: 'fulfilled' as const, value }))
+                  .catch((reason) => ({ status: 'rejected' as const, reason })))
+      );
+  }
+
+    /**
+     * Call all MyPromises and return first resolve/reject (depend on which is first)
+     * @param arrayPromises. Array of MyPromises instance
+     * @returns A new MyPromise
+     */
+  static race<U>(arrayPromises: MyPromise<U>[]) {
       return new MyPromise((resolve, reject) => {
+          arrayPromises.forEach(promise => {
+              MyPromise.resolve(promise)
+                  .then(resolve)
+                  .catch(reject)
+          })
+      })
+  }
+
+  static resolve<U>(value: U | PromiseLike<U>) {
+      return new MyPromise<U>(resolve => {
+          resolve(value);
+      })
+  }
+
+  static reject(reason?: any) {
+      return new MyPromise((_, reject) => {
+          reject(reason);
+      })
+  }
+
+  public then = <U>(
+      thenCallback?: (value: T) => U | PromiseLike<U>,
+      catchCallback?: (reason?: any) => void
+  ) => {
+      const promise = new MyPromise<U>((resolve, reject) => {
           this.thenCbs.push([thenCallback, catchCallback, resolve, reject]);
       });
+
+      this.processPipeline();
+      return promise;
   };
 
-  public catch = (catchCallback: (reason?: any) => void) => {
-      return new MyPromise((resolve, reject) => {
+  public catch = <U>(catchCallback?: (reason?: any) => U) => {
+      const promise = new MyPromise<U>((resolve, reject) => {
           this.thenCbs.push([undefined, catchCallback, resolve, reject]);
       });
+
+      this.processPipeline();
+      return promise;
   };
 
   private resolve = (value: T | PromiseLike<T>) => {
@@ -62,14 +140,19 @@ class MyPromise<T> {
 
           const thenCbs = this.thenCbs;
           this.thenCbs = [];
+
           thenCbs.forEach(([thenCb, catchCb, resolve, reject]) => {
               try {
                   if (this.status === 'fulfilled') {
                       const value = thenCb ? thenCb(this.value) : this.value;
                       resolve(value);
                   } else {
-                      const reason = catchCb ? catchCb(this.error) : this.error;
-                      resolve(reason);
+                      if (catchCb) {
+                          const value = catchCb(this.error);
+                          resolve(value);
+                      } else {
+                          reject(this.error);
+                      }
                   }
               } catch (error) {
                   reject(error);
@@ -79,25 +162,44 @@ class MyPromise<T> {
   }
 }
 
-// const wait = (ms: number) => (
-//     new MyPromise<void>(resolve => {
-//         setTimeout(() => {
-//             resolve();
-//         }, ms);
-//     })
-// )
+const promis1 = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10)
+    }, 500);
+});
 
-new MyPromise((resolve, reject) => {
-    resolve("SECOND");
-}).then(console.log)
+const promis2 = new MyPromise((resolve, reject) => {
+    resolve(5)
+});
 
-console.log('FIRST')
+const promis3 = new MyPromise((resolve, reject) => {
+    reject("ERror");
+});
 
-// const waitedPromise = new Promise<number>((resolve, reject) => {
+MyPromise.race([
+    promis1,
+    promis2
+]).then(console.log);
+
+// MyPromise.all([
+//     promis1,
+//     promis2,
+//     promis3
+// ])
+//     .then(console.log)
+//     .catch((error) => console.log(error))
+
+// const waitedPromise = new MyPromise<number>((resolve, reject) => {
 //     resolve(5);
 // })
+//     .catch(value => {
+//
+//     })
 //     .then((value) => {
 //         console.log("value:", value);
+//     })
+//     .catch((reason) => {
+//         console.log("reason:", reason);
 //     })
 //     .then(() => {
 //         console.log("====");
@@ -105,61 +207,3 @@ console.log('FIRST')
 
 // output
 // reference
-
-// const referencePromise = new Promise((resolve, reject) => {
-//   console.log("RESOLVE_1");
-//   setTimeout(() => {
-//     resolve("RESOLVE_2");
-//   }, 500);
-// })
-//   .then((value) => {
-//     console.log(value);
-//     throw new Error("error");
-//   })
-//   .then((value) => {
-//     console.log("TEST");
-//     return "TEST_2";
-//   })
-//   .then((value) => {
-//     console.log(value);
-//     return "TEST_3";
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
-//   .then(() => {
-//     return new Promise((resolve) => {
-//       resolve(5);
-//     });
-//   })
-//   .then(console.log);
-
-// polyfill
-
-// const polyfillPromise = new MyPromise((resolve, reject) => {
-//   console.log("RESOLVE_1");
-//   setTimeout(() => {
-//     resolve("RESOLVE_2");
-//   }, 500);
-// })
-//   .then((value) => {
-//     console.log(value);
-//     throw new Error("error");
-//   })
-//   .then((value) => {
-//     console.log("TEST");
-//     return "TEST_2";
-//   })
-//   .then((value) => {
-//     console.log(value);
-//     return "TEST_3";
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   })
-//   .then(() => {
-//     return new Promise((resolve) => {
-//       resolve(5);
-//     });
-//   })
-//   .then(console.log);
